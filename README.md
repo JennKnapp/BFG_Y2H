a426e3ed05ea275ef560f5e5c4b609a7485b06b6
ryogali <ryogali@hotmail.com>
2018-07-10 11:13:02 -0400

    edited readme

---
+++ README.md
@@ -0,0 +1,10 @@
+
+0. Creating fasta file and build index
+If you want to create fasta file from csv file run the following command:
+
+```python main.py --create example_AD.csv example_DB.csv ./path_to_fasta/```
+
+If the fasta files already exist, you can build index by providing the fasta path:
+
+``` python main.py --fasta ./path_to_fasta/ ```
+
--- main.py
+++ main.py
@@ -9,10 +9,12 @@ if __name__ == "__main__":
     # make fasta from summary file (AD and DB)
     # if no summary file provided, skip this step
     parser.add_argument('--create', help="Summary file for making referece fasta", nargs=3)
-    
+    parser.add_argument('--fasta'. help="Path to fasta file")
 
     args = parser.parse_args()
     summary = args.create
+    
+    fasta_output = None
 
     if summary is not None:
         for f in summary:
@@ -21,11 +23,15 @@ if __name__ == "__main__":
             elif "DB" in f:
                 DB_summary = f
             else:
-                output = f
+                fasta_output = f
         
-        create_fasta(AD_summary, DB_summary, output, group_spec=True, AD="G1", DB="G4")
+        create_fasta(AD_summary, DB_summary, fasta_output, group_spec=True, AD="G1", DB="G4")
 
-        list_fasta = os.listdir(output)
+    if fasta_output is None:
+        fasta_output = args.fasta
+
+    if fasta_output is not None:
+        list_fasta = os.listdir(fasta_output)
         for fasta in list_fasta:
             build_index(os.path.join(output,fasta), output)
 
