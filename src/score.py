2ccf8d0eb296699235732862117c96a0a30050e2
ryogali <ryogali@hotmail.com>
2018-09-25 14:24:00 -0400

    organized

--- src/score.py
+++ src/score.py
@@ -48,26 +48,18 @@ def rename(df):
     return df.rename(columns = lambda x : str(x)[:-5], index = lambda x : str(x)[:-5])
 
 
-def mcc(l, range_test):
-    PRMCC = []
-    # each list consist of sorted scores
-    for i in range(range_test):
-        temp_screen = l[i:]
-        temp_ns = l[i+1:]
+def calculate_freq(GFP_pre, GFP_high, GFP_med):
+    
+    # for GFP_pre
+    row_freq, col_freq = marginal_freq(GFP_pre)
 
-        precision = sum(temp_screen) / i * 100
-        recall = sum(temp_screen)/sum(l) *100
+    # for GFP_med
+    med_freq = freq(GFP_med)
 
-        TP=sum(temp_screen)
+    # for GFP_high
+    high_freq = freq(GFP_high)
 
-        FN=sum(temp_ns)
-
-
-        MCC= (TP*TN-FP*FN)/math.sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN))*100;
-        PRMCC.append([precision, recall, MCC])
-
-    return PRMCC
-
+    return row_freq, col_freq, med_freq, high_freq
 
 def plot_prc(precision, recall, output_file):
 
@@ -80,32 +72,105 @@ def plot_prc(precision, recall, output_file):
     plt.close()
 
 
+def get_mcc(dicts, gold_st, output_name):
+    # dicts contains intereactions and scores for each index 
+    # compare to gold standard and plot PRC
+    mcc = []
+    for i in dicts.keys():
+        # convert i to df
+        # (AD, DB): score
+        sort_is = pd.Series(dicts[i]).reset_index()
+        # convert values to numbers
+        sort_is[["Score"]] = sort_is[["Score"]].apply(pd.to_numeric)
+        # split bc and orf name
+        sort_is[["AD","AD_BC"]] = sort_is["AD_name"].str.split("_",expand=True)
+        sort_is[["DB","DB_BC"]] = sort_is["DB_name"].str.split("_",expand=True)
 
+        # merge for interactions
+        sort_is["Interactions"] = sort_is[["AD", "DB"]].apply(lambda x: "-".join(x),axis=1)
+
+        # compare gold with our set
+        sort_is["ishit_interaction"] = sort_is.Interactions.isin(gold_st.Interactions).astype(int)
+
+        ## build screen set
+        # AD has to appear in AD_GOLD at least once
+        # DB has to appear in DB_GOLD at least once
+        sort_is["is_AD"] = sort_is.AD.isin(AD_GOLD)
+        sort_is["is_DB"] = sort_is.DB.isin(DB_GOLD)
+
+        # frequencies should be greater than floow
+        sort_is = pd.merge(sort_is, AD_freq, on="AD_name")
+        sort_is = pd.merge(sort_is, DB_freq, on="DB_name")
+
+        sort_is.rename(columns={"0_x":"AD_mfreq", "0_y":"DB_mfreq"}, inplace=True)
+        sort_is["AD_cut"] = sort_is.AD_mfreq > row_cut
+        sort_is["DB_cut"] = sort_is.DB_mfreq >col_cut
+        
+        # get union
+        sort_is["screen"] = (sort_is.is_AD & sort_is.is_DB & sort_is.AD_cut & sort_is.DB_cut).astype(int)
+
+        sort_is = sort_is.sort_values(by="Score", ascending=False)  
+        sort_is = sort_is.reset_index(drop=True)
+        
+        # hit
+        y_pred = sort_is.ishit_interaction.tolist()
+        # screen 
+        y_lab = sort_is.screen.tolist()
+        # scores
+        y_score = sort_is.Score.tolist()
+
+
+        # get confusion matrix
+        cnf_matrix = confusion_matrix(sort_is.screen.tolist(), sort_is.ishit_interaction.tolist())
+        tn, fp, fn, tp = cnf_matrix.ravel()
+        precision, recall, _ = precision_recall_curve(y_pred, y_score)
+        mcc_s = matthews_corrcoef(y_lab, y_pred)
+        
+
+        # make a plot
+
+        plot_prc(precision, recall, output_name)
+
+    
+        mcc.append((i, mcc_s))
+
+    return mcc
+
+
+def test_index(IS_normed, all_pairs, mix_index):
+
+    dicts = {"is_{}".format(i): {} for i in mix_index}
+    for pair in all_pairs:
+        # 0 is AD ; 1 is DB
+        # get this pair from IS_normed
+        s = IS_normed.loc[[pair[0]+"_BC-1",pair[0]+"_BC-2"], [pair[1]+"_BC-1", pair[1]+"_BC-2"]]
+        s = s.unstack().reset_index()
+        s.columns = ["DB","AD", "Score"]
+        scores = s.sort_values(by="Score", ascending=False).reset_index(drop=True)
+
+        for i in mix_index:
+            d_name = "is_{}".format(i)
+            try:
+                s = scores.loc[i,:]
+            except Exception:
+                continue
+
+            if str(s.Score) != "nan":
+                dicts[d_name][(s.AD, s.DB)] = s.Score
+
+    return dicts
 
 
 def score_main(GFP_pre, GFP_high, GFP_med, weights, floor_perc, yi_gold, output):
-    
-    # output directory
-    d_name = os.path.join(output, 
-    
-    
-    
-    
+        
     # to find optimum set of parameters
     # we test all possible combinations
     # generates a list of tuples containing all possible combinations
     l = [weights, floor_perc]
     comb = list(itertools.product(*l))
     
-    # for GFP_pre
-    row_freq, col_freq = marginal_freq(GFP_pre)
-   
-    # for GFP_med
-    med_freq_renamed = rename(freq(GFP_med)) 
-    med_freq = freq(GFP_med)
-    # for GFP_high
-    #high_freq = rename(freq(GFP_high))
-    high_freq = freq(GFP_high)
+    # get frequencies
+    row_freq, col_freq, med_freq, high_freq = calculate_freq(GFP_pre, GFP_high, GFP_med)
     
     shape = GFP_pre.shape
     total_rows = shape[0] #AD
@@ -128,6 +193,8 @@ def score_main(GFP_pre, GFP_high, GFP_med, weights, floor_perc, yi_gold, output)
     AD_intersect = list(set(AD_GOLD) & set(AD_NAMES))
     DB_intersect = list(set(DB_GOLD) & set(DB_NAMES))
     mix_index = [0,1,2,3]
+    
+    # optimization
     for s in comb:
         weight = s[0]
         floor = s[1]
@@ -164,119 +231,16 @@ def score_main(GFP_pre, GFP_high, GFP_med, weights, floor_perc, yi_gold, output)
         
         # test which set of bc we want to use
         # in this case, we will have max 4 min 1 score(s) for each orf pair
-        dicts = {"is_{}".format(i): {} for i in mix_index}
+        dicts = test_index(IS_normed, all_pairs, mix_index)
         
-        all_pairs = list(itertools.product(AD_intersect, DB_intersect))
-        print(datetime.datetime.now()) 
-        for pair in all_pairs:
-            # 0 is AD ; 1 is DB
-            # get this pair from IS_normed
-            
-            s = IS_normed.loc[[pair[0]+"_BC-1",pair[0]+"_BC-2"], [pair[1]+"_BC-1", pair[1]+"_BC-2"]]
-            s = s.unstack().reset_index()
-            s.columns = ["DB","AD", "Score"]
-            scores = s.sort_values(by="Score", ascending=False).reset_index(drop=True)
-            
-            for i in mix_index:
-                d_name = "is_{}".format(i)
-                try:
-                    s = scores.loc[i,:]
-                except Exception:
-                    continue    
-            
-                if str(s.Score) != "nan":
-                    dicts[d_name][(s.AD, s.DB)] = s.Score
-        
-        print(datetime.datetime.now())
         AD_freq = AD_freq.to_frame()
         DB_freq = DB_freq.to_frame()
         
         AD_freq["AD_name"] = AD_freq.index
         DB_freq["DB_name"] = DB_freq.index
-        for i in dicts.keys(): # calculate MCC for each index
         
-            # convert i to df 
-            # (AD, DB): score 
-            sort_is = pd.Series(dicts[i]).reset_index()
-            sort_is.columns = ["AD_name", "DB_name", "Score"]
-            
-            # convert values to numbers
-            sort_is[["Score"]] = sort_is[["Score"]].apply(pd.to_numeric)
-        
-            # split bc and orf name
-            sort_is[["AD","AD_BC"]] = sort_is["AD_name"].str.split("_",expand=True)
-            sort_is[["DB","DB_BC"]] = sort_is["DB_name"].str.split("_",expand=True)
-        
-            # merge for interactions
-            sort_is["Interactions"] = sort_is[["AD", "DB"]].apply(lambda x: "-".join(x),axis=1)
 
-            # how many interactions we have?
-            print sort_is.shape
-            
-            # compare gold with our set
-            sort_is["ishit_interaction"] = sort_is.Interactions.isin(gold_st.Interactions).astype(int)
-            
-            # print hit.shape 
-            # filter the sort_is
-            # AD has to appear in AD_GOLD at least once
-            # union of AD in our set & AD in GOLD 
-            # DB has to appear in DB_GOLD at least once
-            # --> both col has to be in the union set
-        
-            # screen = sort_is.loc[sort_is.AD.isin(AD_GOLD) | sort_is.DB.isin(DB_GOLD)]
-            
-            sort_is["is_AD"] = sort_is.AD.isin(AD_GOLD)
-            sort_is["is_DB"] = sort_is.DB.isin(DB_GOLD)
-            
-            sort_is = pd.merge(sort_is, AD_freq, on="AD_name")
-            sort_is = pd.merge(sort_is, DB_freq, on="DB_name")
-
-            sort_is.rename(columns={"0_x":"AD_mfreq", "0_y":"DB_mfreq"}, inplace=True)
-
-            #sort_is = screen.loc[(screen.AD_mfreq > row_cut) & (screen.DB_mfreq >col_cut)]
-            
-            sort_is["AD_cut"] = sort_is.AD_mfreq > row_cut
-            sort_is["DB_cut"] = sort_is.DB_mfreq >col_cut
-            
-            sort_is["screen"] = (sort_is.is_AD & sort_is.is_DB & sort_is.AD_cut & sort_is.DB_cut).astype(int)
-
-            sort_is = sort_is.sort_values(by="Score", ascending=False)  
-            sort_is = sort_is.reset_index(drop=True)
-            
-            # hit set 
-            y_pred = sort_is.ishit_interaction.tolist()
-            # screen set
-            y_lab = sort_is.screen.tolist()
-
-            # scores
-            y_score = sort_is.Score.tolist()
-            
-            
-            # get confusion matrix
-            cnf_matrix = confusion_matrix(sort_is.screen.tolist(), sort_is.ishit_interaction.tolist())
-            print sum(sort_is.ishit_interaction.tolist())
-            #print sort_is.shape
-            print cnf_matrix
-            tn, fp, fn, tp = cnf_matrix.ravel()
-            print tn, fp, fn, tp
-            
-            precision, recall, _ = precision_recall_curve(y_pred, y_score)
-            mcc = matthews_corrcoef(y_lab, y_pred)
-            print precision
-            print recall
-            print mcc
-            output_file = "/home/rothlab/rli/www/html/test.png"
-            plot_prc(precision, recall, output_file)
-
-            #print(datetime.datetime.now())
-            #print screen
-            #print screen.shape
-            # from pre matrix, select those pairs 
-            # build a screen set
-            # If presort score sucks for this pair?
-            # compare to gold standard
-            # find intersections
-            break
+        mcc_list = get_mcc(dicts, gold_st, output_name)
         break
 
 if __name__ == "__main__":
